efa3fffca053f50798e0266a5d31789b723caec9
Bacra <bacra.woo@gmail.com>
2017-08-16 22:46:33 +0800

    Split file

--- test/test_i18n_function_parser.js
+++ test/test_i18n_function_parser.js
@@ -1,13 +1,13 @@
 var expect				= require('expect.js');
 var esprima				= require('esprima');
-var i18nFunctionUtils	= require('../lib/i18n_function_utils');
+var i18nFunctionParser	= require('../lib/i18n_function_parser');
 
-describe('#i18n_function_utils', function()
+describe('#i18n_function_parser', function()
 {
 	it('#dealTranslateDataJSON', function()
 	{
 		var astData = require('./files/translate_data_json.json');
-		var result = i18nFunctionUtils._dealTranslateDataJSON(astData);
+		var result = i18nFunctionParser._dealTranslateDataJSON(astData);
 		console.log('\n\n\n', result);
 		expect(result).to.eql(require('./files/translate_data_json_output.json'));
 	});
@@ -16,7 +16,7 @@ describe('#i18n_function_utils', function()
 	{
 		var code = require('./files/i18n_handler_example').toString();
 		var ast = esprima.parse(code);
-		var result = i18nFunctionUtils.parse(ast);
+		var result = i18nFunctionParser.parse(ast);
 
 		expect(result).to.eql(require('./files/i18n_handler_example_output.json'));
 	});
