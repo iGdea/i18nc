85e69ef65758be2b65b0be89a41e8464427b8fa4
Bacra <bacra.woo@gmail.com>
2017-08-18 20:14:38 +0800

    Update: escodegen style

--- test/files/i18n_handler_example_i18nc_wdithcode_output_code.js
+++ test/files/i18n_handler_example_i18nc_wdithcode_output_code.js
@@ -48,17 +48,17 @@ function I18N(msg, subtype)
 		 * }
 		 */
 		var __TRANSLATE_JSON__ = {
-    'zh': {
-        'DEFAULTS': {
-            '中文0': 'indb <thisfile> db2' || 'indb <thisfile> custom' || 'indb <thisfile> db1',
-            '中文1': 'in_file custom1' || 'in_file zh1',
-            '中文2': [] || 'in_file zh2',
-            '中文4_empty': [] || [],
-            '中文5_empty': [] || [],
-            '中文6_empty': 'in_file 4' || 'in_file 1'
-        }
-    }
-};
+			'zh': {
+				'DEFAULTS': {
+					'中文0': 'indb <thisfile> db2' || 'indb <thisfile> custom' || 'indb <thisfile> db1',
+					'中文1': 'in_file custom1' || 'in_file zh1',
+					'中文2': [] || 'in_file zh2',
+					'中文4_empty': [] || [],
+					'中文5_empty': [] || [],
+					'中文6_empty': 'in_file 4' || 'in_file 1'
+				}
+			}
+		};
 
 		self.__TRANSLATE_LAN__ = LAN;
 		self.__TRANSLATE_LAN_JSON__ = __TRANSLATE_JSON__[LAN] || {};
