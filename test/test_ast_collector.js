ce5764372b442b8787ce71a88a5eea6b66673f4a
Bacra <bacra.woo@gmail.com>
2017-08-21 17:16:40 +0800

    Add: `skip i18nc sacn` expression

---
+++ test/test_ast_collector.js
@@ -0,0 +1,7 @@
+var expect			= require('expect.js');
+var AstCollector	= require('../lib/ast_collector').AstCollector;
+
+describe('#AstCollector', function()
+{
+	it('#ignore scan', function(){});
+});
