ccc42cc0944b9cd757b5b2501b5629388cf1b32d
Bacra Woo <bacra.woo@gmail.com>
2017-08-23 02:50:24 +0800

    Update: blank replace

--- test/test_i18n_function_parser.js
+++ test/test_i18n_function_parser.js
@@ -9,6 +9,9 @@ describe('#i18n_function_parser', function()
 	{
 		var astData = require('./files/translate_data_ast.json');
 		var result = i18nFunctionParser._translateDataAst2json(astData);
+
+		// autoWriteFile('translate_data.json', JSON.stringify(result, null, '\t'));
+
 		expect(result).to.eql(require('./files/translate_data.json'));
 	});
 
