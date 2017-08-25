39f8a5a628145f506fd2fd55ecd39bec35947e01
Bacra <bacra.woo@gmail.com>
2017-08-25 11:04:28 +0800

    Add: ast empty head test

--- test/test_i18n_function_parser.js
+++ test/test_i18n_function_parser.js
@@ -5,14 +5,27 @@ var i18nFunctionParser	= require('../lib/i18n_function_parser');
 
 describe('#i18n_function_parser', function()
 {
-	it('#translateDataAst2json', function()
+	describe('#translateDataAst2json', function()
 	{
-		var astData = require('./files/translate_data_ast.json');
-		var result = i18nFunctionParser._translateDataAst2json(astData);
+		it('#normal', function()
+		{
+			var astData = require('./files/translate_data_ast.json');
+			var result = i18nFunctionParser._translateDataAst2json(astData);
 
-		// autoWriteFile('translate_data.json', JSON.stringify(result, null, '\t'));
+			autoWriteFile('translate_data.json', JSON.stringify(result, null, '\t'));
 
-		expect(result).to.eql(require('./files/translate_data.json'));
+			expect(result).to.eql(require('./files/translate_data.json'));
+		});
+
+		it('#empty head', function()
+		{
+			var astData = require('./files/translate_data_ast_empty_head.json');
+			var result = i18nFunctionParser._translateDataAst2json(astData);
+
+			autoWriteFile('translate_data_empty_head.json', JSON.stringify(result, null, '\t'));
+
+			expect(result).to.eql(require('./files/translate_data_empty_head.json'));
+		});
 	});
 
 	it('#parse', function()
