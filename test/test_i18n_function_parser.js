285d9ee1fe8f8cbb7c4e87410b3dd05314dd272b
Bacra <bacra.woo@gmail.com>
2017-08-30 22:16:07 +0800

    Update: `loadTranslateJSONByAst` return format

--- test/test_i18n_function_parser.js
+++ test/test_i18n_function_parser.js
@@ -5,12 +5,12 @@ var i18nFunctionParser	= require('../lib/i18n_function_parser');
 
 describe('#i18n_function_parser', function()
 {
-	describe('#translateDataAst2json', function()
+	describe('#translateSubtreeAst2json', function()
 	{
 		it('#normal', function()
 		{
 			var astData = require('./files/translate_data_ast.json');
-			var result = i18nFunctionParser._translateDataAst2json(astData);
+			var result = i18nFunctionParser._translateSubtreeAst2json(astData);
 
 			autoWriteFile('translate_data.json', JSON.stringify(result, null, '\t'));
 
@@ -20,7 +20,7 @@ describe('#i18n_function_parser', function()
 		it('#empty head', function()
 		{
 			var astData = require('./files/translate_data_ast_empty_head.json');
-			var result = i18nFunctionParser._translateDataAst2json(astData);
+			var result = i18nFunctionParser._translateSubtreeAst2json(astData);
 
 			autoWriteFile('translate_data_empty_head.json', JSON.stringify(result, null, '\t'));
 
