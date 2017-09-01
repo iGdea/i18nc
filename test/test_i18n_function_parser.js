1c167f3d93b8b98632752fbbc56c70aa37dfcb46
Bacra <bacra.woo@gmail.com>
2017-09-01 21:46:33 +0800

    Fix: empty parse

--- test/test_i18n_function_parser.js
+++ test/test_i18n_function_parser.js
@@ -5,6 +5,66 @@ var i18nFunctionParser	= require('../lib/i18n_function_parser');
 
 describe('#i18n_function_parser', function()
 {
+	describe('#translateLogicalValAst2arr', function()
+	{
+		function run(code)
+		{
+			var ast = esprima.parse(code).body[0].expression;
+			return i18nFunctionParser._translateLogicalValAst2arrWidthClear(ast);
+		}
+
+		it('#normal', function()
+		{
+			expect(run('1 || 2 || 3 || 4')).to.eql([4, 3, 2, 1]);
+		});
+
+		it('#no and', function()
+		{
+			expect(run).withArgs('1 || 2 && 3 || 4').to.throwError('RANSLATE_JSON DATA OPERATOR IS NOT `OR`');
+		});
+
+		it('#array empty', function()
+		{
+			expect(run('[] || 0 || 0')).to.eql(['', 0, 0]);
+		});
+
+		it('#empty string', function()
+		{
+			expect(run('"" || 1 || 0')).to.eql([0, 1]);
+		});
+
+		it('#empty 10000', function()
+		{
+			expect(run('1 || 0 || 0 || 0')).to.eql([0, 0, 0, 1]);
+		});
+
+		it('#empty 0001', function()
+		{
+			expect(run('0 || 0 || 0 || 1')).to.eql([1]);
+		});
+
+		it('#empty 0(00)1', function()
+		{
+			expect(run('0 || (0 || 0) || 1')).to.eql([1]);
+		});
+
+		it('#empty 1001', function()
+		{
+			expect(run('1 || 0 || 0 || 1')).to.eql([1, 0, 0, 1]);
+		});
+
+		it('#empty 1(00)1', function()
+		{
+			expect(run('1 || (0 || 0) || 1')).to.eql([1, 0, 0, 1]);
+		});
+
+		it('#empty 10(01)', function()
+		{
+			expect(run('1 || 0 || (0 || 1)')).to.eql([1, 0, 0, 1]);
+		});
+	});
+
+
 	describe('#translateSubtreeAst2json', function()
 	{
 		it('#normal', function()
