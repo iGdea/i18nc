1e0ce2e366f5083ca8c303a6a525f317d586ca22
Bacra Woo <bacra.woo@gmail.com>
2018-02-09 03:30:46 +0800

    Publish: v4.1.0

--- test/test_options.js
+++ test/test_options.js
@@ -13,5 +13,7 @@ describe('#options', function()
 
 		expect(newOptions.I18NHandlerName).to.be('I18N');
 		expect(newOptions.codeModifiedArea).to.eql(['I18NHandler', 'translateWord', 'I18NHandlerAlias']);
+		expect(optionsUtils.extend().I18NHandlerName).to.be('I18N');
+		expect(optionsUtils.extend({somekey: true}).somekey).to.be(undefined);
 	});
 });
