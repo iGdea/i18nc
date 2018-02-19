6cb3d8a0c66ba066a1117c5095e8b743329d5cd6
Bacra Woo <bacra.woo@gmail.com>
2018-02-20 02:26:08 +0800

    Update: split test_main_options.s

---
+++ test/test_i18n_func_update.js
@@ -0,0 +1,46 @@
+var expect = require('expect.js');
+
+describe('#i18n_func_update', function()
+{
+	describe('#check', function()
+	{
+		it('#partial', function()
+		{
+			console.log('@todo');
+		});
+	});
+
+
+	describe('#update', function()
+	{
+		it('#partial', function()
+		{
+			console.log('@todo');
+		});
+
+		it('#orignal', function()
+		{
+			console.log('@todo');
+		});
+
+		it('#empty', function()
+		{
+			console.log('@todo');
+		});
+
+		it('#simple', function()
+		{
+			console.log('@todo');
+		});
+
+		it('#complete', function()
+		{
+			console.log('@todo');
+		});
+
+		it('#simple2complete', function()
+		{
+			console.log('@todo');
+		});
+	});
+});
