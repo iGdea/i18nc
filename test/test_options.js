a9e3804bde8742a1848d23a09dd36e597bd88be0
Bacra Woo <bacra.woo@gmail.com>
2018-02-24 21:33:04 +0800

    Add: originalOptions test for extend

--- test/test_options.js
+++ test/test_options.js
@@ -15,5 +15,8 @@ describe('#options', function()
 		expect(newOptions.codeModifiedArea).to.eql(['I18NHandler', 'translateWord', 'I18NHandlerAlias']);
 		expect(optionsUtils.extend().I18NHandlerName).to.be('I18N');
 		expect(optionsUtils.extend({somekey: true}).somekey).to.be(undefined);
+
+		var originalOptions = {somekey: true};
+		expect(optionsUtils.extend(originalOptions).originalOptions).to.be(originalOptions);
 	});
 });
