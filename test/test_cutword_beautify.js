bdf050ed828f6cb5a9449c7bdc4c11305b97478a
Bacra Woo <bacra.woo@gmail.com>
2018-03-05 19:52:30 +0800

    Add: check newVal when cutword by end symbols

--- test/test_cutword_beautify.js
+++ test/test_cutword_beautify.js
@@ -46,7 +46,7 @@ describe('#cutword_beautify', function()
 		it('#split symbol', function()
 		{
 			expect(txt2code('你好！出发了吗？第一点；第二点。出彩vs回家.111'))
-				.to.be("I18N('你好！')+I18N('出发了吗？')+I18N('第一点；')+I18N('第二点。')+I18N('出彩vs回家.')+I18N('111')");
+				.to.be("I18N('你好！')+I18N('出发了吗？')+I18N('第一点；')+I18N('第二点。')+I18N('出彩vs回家.')+'111'");
 			expect(txt2code('你好!!出发了吗?不走吗'))
 				.to.be("I18N('你好!!')+I18N('出发了吗?')+I18N('不走吗')");
 			expect(txt2code('你好!!出发了吗?不走吗？'))
