546920b9d25b59adafa6fa7f5055c0a9368fe116
Bacra Woo <bacra.woo@gmail.com>
2018-06-14 19:17:03 +0800

    Update: test file

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -3,22 +3,20 @@
 var Benchmark = require('benchmark');
 
 
-function noop(){}
-
 function getCacheVarByFunc()
 {
 	var self = getCacheVarByFunc;
 	var cache = self.data || (self.data = {});
 	var value = (function(cache){if (cache.gb){return cache.gb.lan}else{cache.gb = {}}})(cache);
-	noop(value);
+	return value;
 }
 
 function getCacheVarByVarAndFunc()
 {
 	var self = getCacheVarByVarAndFunc;
 	var cache = self.data || (self.data = {});
-	var value = (cache.gb && cache.gb.lan) || (function(cache){if (cache.gb){return cache.gb.lan}else{cache.gb = {}}})(cache);
-	noop(value);
+	var value = cache.gb && cache.gb.lan || (function(cache){if (cache.gb){return cache.gb.lan}else{cache.gb = {}}})(cache);
+	return value;
 }
 
 function getCacheVarByVar()
@@ -26,22 +24,22 @@ function getCacheVarByVar()
 	var self = getCacheVarByVar;
 	var cache = self.data || (self.data = {gb: {}});
 	var value = cache.gb && cache.gb.lan;
-	noop(value);
+	return value;
 }
 
 
 var suite = new Benchmark.Suite;
-suite.add('func', function()
+suite.add('var&func', function()
+	{
+		getCacheVarByVarAndFunc();
+	})
+	.add('func', function()
 	{
 		getCacheVarByFunc();
 	})
 	.add('var', function()
 	{
 		getCacheVarByVar();
-	})
-	.add('var&func', function()
-	{
-		getCacheVarByVarAndFunc();
 	})
 	.on('cycle', function(event)
 	{
--- test/files/casefile/i18n_handler/i18n_handler_simple_example.js
+++ test/files/casefile/i18n_handler/i18n_handler_simple_example.js
@@ -1,4 +1,4 @@
 module.exports = I18N;
 function I18N(c,a){
-	var b=I18N;b.__FILE_KEY__='i18n_handler_example';b.__FUNCTION_VERSION__='a';if(!a||!a.join)return''+c;var d=0;return(''+c).replace(/(%s)|(%\{(.+?)\})/g,function(c){var b=a[d++];return b===undefined||b===null?c:b})
+	if(!a||!a.join)return''+c;var b=I18N;b.__FILE_KEY__='i18n_handler_example';b.__FUNCTION_VERSION__='a';var d=0;return(''+c).replace(/(%s)|(%\{(.+?)\})/g,function(c){var b=a[d++];return b===undefined||b===null?c:b})
 }
