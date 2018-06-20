094706eaad700755ce096d63153e01b3a1b1902d
Bacra Woo <bacra.woo@gmail.com>
2018-06-21 02:21:41 +0800

    Clear files

--- test/files/casefile/func_code/func_code_combo_literal.js
+++
@@ -1,7 +0,0 @@
-module.exports = function code()
-{
-	var a = '123'+'中文'+I18N('abc');
-	var b = '123>中文'+I18N('abc');
-	var c = '<span>11</span>55'+'<span>11</span>'+222+'简体'+333+'2<span>22</span>';
-	var d = '1'+'2';
-}
--- test/files/casefile/func_code/func_code_tpl_comment.js
+++
@@ -1,9 +0,0 @@
-exports.has_words = function code()
-{
-	var a = "正文<!-- tpl注释 -->内容";
-};
-
-exports.no_words = function code()
-{
-	var a = "<!-- tpl -->";
-};
