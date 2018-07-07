c9ef20af6575a165d48179bd40fe9e1d2e43c7fd
Bacra Woo <bacra.woo@gmail.com>
2018-07-07 20:31:54 +0800

    Add: replace check

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,15 +1,24 @@
 var Promise = require('bluebird');
+var replaceSuite = require('./suite_replace');
 var tasks =
 [{
 	name: 'varCache',
 	suite: require('./suite_var_cache')
+},
+{
+	name: 'Repalce - check',
+	suite: replaceSuite.noReplace
+},
+{
+	name: 'Replace - do',
+	suite: replaceSuite.doReplace
 }];
 
 Promise.each(tasks, function(item)
 {
 	return new Promise(function(resolve)
 	{
-		console.log( '==== '+item.name+' start ====');
+		console.log('\n\n==== '+item.name+' start ====');
 		item.suite.on('complete', resolve).run();
 	});
 })
