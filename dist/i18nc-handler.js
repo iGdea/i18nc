5a58bfe9e5e008065086cc62e486219c27e8a5dc
Bacra Woo <bacra.woo@gmail.com>
2018-07-18 21:10:08 +0800

    Update: wrap i18n handler

--- dist/i18nc-handler.js
+++ dist/i18nc-handler.js
@@ -1,17 +1,31 @@
-function I18N(msg, tpldata)
+;(function (handler)
 {
-	msg += '';
-	if (!tpldata || !tpldata.length || msg.indexOf('%') == -1) return msg;
-
-	var self = I18N;
-
-	self.K = '*';
-	self.V = 'b.s';
-
-	var replace_index = 0;
-	return msg.replace(/%s|%\{.+?\}/g, function(all)
+	if (typeof define == 'function')
+		define(handler);
+	else if (typeof module != 'undefined' && exports != 'undefined' && module.exports === exports)
+		handler(null, exports);
+	else
+		handler(null, this);
+})
+(function(r, ctx)
+{
+	function I18N(msg, tpldata)
 	{
-		var newVal = tpldata[replace_index++];
-		return newVal === undefined || newVal === null ? all : newVal;
-	});
-}
+		msg += '';
+		if (!tpldata || !tpldata.length || msg.indexOf('%') == -1) return msg;
+	
+		var self = I18N;
+	
+		self.K = '*';
+		self.V = 'b.s';
+	
+		var replace_index = 0;
+		return msg.replace(/%s|%\{.+?\}/g, function(all)
+		{
+			var newVal = tpldata[replace_index++];
+			return newVal === undefined || newVal === null ? all : newVal;
+		});
+	}
+
+	ctx.I18N = I18N;
+});
---
+++ test/build/wrap-code.tpl.js
@@ -0,0 +1,11 @@
+/* global define module exports */
+
+module.exports = function(handler)
+{
+	if (typeof define == 'function')
+		define(handler);
+	else if (typeof module != 'undefined' && exports != 'undefined' && module.exports === exports)
+		handler(null, exports);
+	else
+		handler(null, this);
+};
