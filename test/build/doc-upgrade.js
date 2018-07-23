d5b1d66efc7a5a77ba42dfcf78c0555d1b736b0e
Bacra Woo <bacra.woo@gmail.com>
2018-07-23 10:41:52 +0800

    Update: sort options keys for upgrade.md

--- test/build/doc-upgrade.js
+++ test/build/doc-upgrade.js
@@ -8,20 +8,24 @@ var depdOptions = require('../../lib/upgrade/depd_options');
 var maxNewKeyLen = 0;
 var maxOldKeyLen = 0;
 var tableContentArr = _.map(depdOptions.OPTIONS_OLDKEY_MAP, function(newKey, oldKey)
-{
-	if (newKey.length > maxNewKeyLen) maxNewKeyLen = newKey.length;
-	if (oldKey.length > maxOldKeyLen) maxOldKeyLen = oldKey.length;
-	return [oldKey, newKey];
-});
+	{
+		if (newKey.length > maxNewKeyLen) maxNewKeyLen = newKey.length;
+		if (oldKey.length > maxOldKeyLen) maxOldKeyLen = oldKey.length;
+		return {old: oldKey, new: newKey};
+	})
+	.sort(function(a, b)
+	{
+		return a.old.toLowerCase() > b.old.toLowerCase() ? 1 : -1;
+	});
 var newKeyStr = new Array(maxNewKeyLen+1).join(' ');
 var oldKeyStr = new Array(maxOldKeyLen+1).join(' ');
 tableContentArr = tableContentArr.map(function(item)
 {
-	return '| '+oldKeyStr.substr(item[0].length)+item[0]
-		+' | '+item[1]+newKeyStr.substr(item[1].length)
+	return '| '+oldKeyStr.substr(item.old.length)+item.old
+		+' | '+item.new+newKeyStr.substr(item.new.length)
 		+' |';
 });
-var tableContent = '| 老版本'+oldKeyStr.substr(5)
+var tableContent = '| '+oldKeyStr.substr(5)+'老版本'
 	+' | 新版本'+newKeyStr.substr(5)
 	+' |\n'
 	+'|'+new Array(maxOldKeyLen+2).join('-')+':'
