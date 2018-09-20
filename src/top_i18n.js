16fb64403601139536fcfee2b3d9ccc32c7d6be3
Bacra <bacra.woo@gmail.com>
2018-09-21 06:46:27 +0800

    Update: topI18N handler for new json style

--- src/top_i18n.js
+++ src/top_i18n.js
@@ -2,7 +2,87 @@
 
 'use strict';
 
-module.exports = function topI18N(msg, args, translateJSON, fileKey, data, handler)
+/* eslint-disable */
+function topI18N(msg, args, translateJSON, fileKey, data, handler)
+{
+	var funcVersion = handler.V;
+	if (!funcVersion || funcVersion.toUpperCase().charCodeAt(0) >= 'G'.charCodeAt(0))
+		return topI18N_v2.apply(this, arguments);
+	else
+		return topI18N_v1.apply(this, arguments);
+}
+/* eslint-enable */
+
+
+function topI18N_v2(msg, args, translateJSON, fileKey, data, handler)
+{
+	var self = handler,
+		tpldata = args[1],
+		subtype = args[2],
+		replace_index = 0,
+		lanIndexReverseArr, i, lanIndex, msgResult, translateValues,
+		LAN = $I18N_getLanguageCode(data);
+
+	if (!tpldata || !tpldata.join)
+	{
+		subtype = tpldata;
+		tpldata = [];
+	}
+
+	if (LAN && LAN.split)
+	{
+		if (self.L != LAN)
+		{
+			var dblans = translateJSON.$,
+				dblansMap = {},
+				lanKeys = LAN.split(',');
+			lanIndexReverseArr = self.M = [];
+			for(i = dblans.length; i--;)
+			{
+				dblansMap[dblans[i]] = i;
+			}
+			for(i = lanKeys.length; i--;)
+			{
+				lanIndex = dblansMap[lanKeys[i]];
+				if (lanIndex || lanIndex === 0) lanIndexReverseArr.push(lanIndex);
+			}
+			// 放到最后，防止前面代码出错，导致if逻辑被保存
+			self.L = LAN;
+		}
+
+		lanIndexReverseArr = self.M;
+		var _getVaule = function(subtype)
+		{
+			translateValues = translateJSON[subtype] && translateJSON[subtype][msg];
+			if (translateValues)
+			{
+				msgResult = translateValues[lanIndex];
+				if (typeof msgResult == 'number') msgResult = translateValues[msgResult];
+			}
+		};
+		for(i = lanIndexReverseArr.length; !msgResult && i--;)
+		{
+			lanIndex = lanIndexReverseArr[i];
+			if (subtype) _getVaule(subtype);
+			if (!msgResult) _getVaule('*');
+		}
+
+		if (msgResult) msg = msgResult;
+	}
+
+	msg += '';
+	// 判断是否需要替换：不需要替换，直接返回
+	if (!tpldata.length || msg.indexOf('%') == -1) return msg;
+
+	return msg.replace(/%s|%\{.+?\}/g, function(all)
+	{
+		var newVal = tpldata[replace_index++];
+		return newVal === undefined ? all : newVal === null ? '' : newVal;
+	});
+}
+
+
+function topI18N_v1(msg, args, translateJSON, fileKey, data, handler)
 {
 	var self = handler,
 		tpldata = args[1],
