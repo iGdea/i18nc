3e72fe4c35b28d0756dc9e082a4d69967f4a2e56
Bacra <bacra.woo@gmail.com>
2018-09-21 06:29:26 +0800

    Add: test for new gener

---
+++ test/files/output/i18n_func_generator/merge_translate_data_output.js
@@ -0,0 +1,20 @@
+module.exports = function textWrapCode(){
+
+var json = {
+	'$': [
+		'en-US',
+		'zh-TW'
+	],
+	'*': {
+		'%s美好%s生活': ['%sgood%s life'],
+		'%{中文}词典': ['%{Chinese} dictionary'],
+		'空白': [[]],
+		'简体': [
+			'simplified',
+			'簡體'
+		]
+	},
+	'subtype': { '简体': ['simplified subtype'] }
+}
+
+}
