c83f79b44c49b7c98f5f2fe6db543431eef078f6
Bacra <bacra.woo@gmail.com>
2018-09-25 21:24:33 +0800

    Update: merge date style

--- test/test_allplatform/test_merge_transalte_data.js
+++ test/test_allplatform/test_merge_transalte_data.js
@@ -4,17 +4,22 @@ var expect				= require('expect.js');
 var autoTestUtils		= require('../auto_test_utils');
 var requireAfterWrite	= autoTestUtils.requireAfterWrite('merge_translate_data');
 var mergeTranslateData	= require('../../lib/i18n_func/merge_translate_data');
+var mergeArgsData		= require('../files/merge_translate_data');
 
 
 describe('#mergeTranslateData', function()
 {
 	it('#base', function()
 	{
-		var args = require('../files/merge_translate_data');
-		var result = mergeTranslateData(args);
-
+		var result = mergeTranslateData(mergeArgsData);
 		var outputJSON = requireAfterWrite('merge_translate_data.json', result);
+		expect(result).to.eql(outputJSON);
+	});
 
+	it('#merge_data', function()
+	{
+		var result = mergeTranslateData._mergeData(mergeArgsData);
+		var outputJSON = requireAfterWrite('merge_data.json', result);
 		expect(result).to.eql(outputJSON);
 	});
 });
