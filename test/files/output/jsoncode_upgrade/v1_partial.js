49e71e826c7eec76d4497b4b20777045e491424c
Bacra <bacra.woo@gmail.com>
2018-09-25 17:04:07 +0800

    Add: jsoncode_upgrade tests

---
+++ test/files/output/jsoncode_upgrade/v1_partial.js
@@ -0,0 +1,101 @@
+module.exports = function textWrapCode(){
+
+function I18N(msg, tpldata, subtype)
+{
+	var self = I18N;
+	var data = self.$ || (self.$ = {});
+	var LAN = (function(){return global.__i18n_lan__})(data);
+	if (!tpldata || !tpldata.join)
+	{
+		subtype = tpldata;
+		tpldata = [];
+	}
+
+	if (LAN && LAN.split)
+	{
+		var lanArr, i, len, lanItem;
+		if (self.L != LAN)
+		{
+			self.K = 'i18n_handler_example';
+			self.V = 'cf';
+			self.D = {
+				'en-US': {
+					'DEFAULTS': {
+						'%s美好%s生活': '%sgood%s life',
+						'%{中文}词典': '%{Chinese} dictionary',
+						// '无':
+						// '空白':
+						'简体': 'simplified'
+					},
+					'SUBTYPES': { 'subtype': { '简体': 'simplified subtype' } }
+				},
+				'zh-TW': {
+					'DEFAULTS': {
+						// '%s美好%s生活':
+						// '%{中文}词典':
+						'无': '無',
+						// '空白':
+						'简体': '簡體'
+					},
+					'SUBTYPES': {
+						'subtype': {
+							// '简体':
+						}
+					}
+				}
+			};
+
+			var __TRANSLATE_JSON__ = self.D;
+			var lanKeys = LAN.split(',');
+			lanArr = self.M = [];
+			for(i = 0, len = lanKeys.length; i < len; i++)
+			{
+				lanItem = __TRANSLATE_JSON__[lanKeys[i]];
+				if (lanItem) lanArr.push(lanItem);
+			}
+			self.L = LAN;
+		}
+
+		lanArr = self.M;
+		var resultDefault, resultSubject, allsubtypes, alldefaults, subtypeJSON;
+		for(i = 0, len = lanArr.length; i < len; i++)
+		{
+			lanItem = lanArr[i];
+			if (subtype)
+			{
+				allsubtypes = lanItem.SUBTYPES;
+				subtypeJSON = allsubtypes && allsubtypes[subtype];
+				resultSubject = subtypeJSON && subtypeJSON[msg];
+				if (resultSubject) break;
+			}
+			if (!resultDefault)
+			{
+				alldefaults = lanItem.DEFAULTS;
+				resultDefault = alldefaults && alldefaults[msg];
+			}
+		}
+
+		if (resultSubject) msg = resultSubject;
+		else if (resultDefault) msg = resultDefault;
+	}
+
+	msg += '';
+	if (!tpldata.length || msg.indexOf('%') == -1) return msg;
+
+	var replace_index = 0;
+	return msg.replace(/%s|%\{.+?\}/g, function(all)
+	{
+		var newVal = tpldata[replace_index++];
+		return newVal === undefined ? all : newVal === null ? '' : newVal;
+	});
+}
+
+I18N("简体")
+I18N("空白")
+I18N("%s美好%s生活")
+I18N("%{中文}词典")
+I18N("无")
+
+I18N("简体", "subtype")
+
+}
--- test/test_allplatform/test_jsoncode_upgrade.js
+++ test/test_allplatform/test_jsoncode_upgrade.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _					= require('lodash');
 var expect				= require('expect.js');
 var i18nc				= require('../../');
 var autoTestUtils		= require('../auto_test_utils');
@@ -10,13 +11,50 @@ describe('#jsoncode upgrade', function()
 	describe('#v1', function()
 	{
 		var code = require('../files/casefile/i18n_handler/i18n_handler_jsoncode_v1').toString();
+		var info = i18nc(code);
+		var funcTranslateWords = info.allFuncTranslateWords();
+		var dbTranslateWords =
+		{
+			version: 2,
+			data:
+			{
+				'*': funcTranslateWords.toJSON()
+			}
+		};
+		var funcTranslateWordsList = funcTranslateWords.words();
+		code += '\n\n'
+			+_.map(funcTranslateWordsList.DEFAULTS, function(val)
+			{
+				return 'I18N("'+val+'")';
+			})
+			.join('\n')
+			+'\n\n'
+			+_.map(funcTranslateWordsList.SUBTYPES, function(vals, subtype)
+			{
+				return vals.map(function(val)
+				{
+					return 'I18N("'+val+'", "'+subtype+'")';
+				})
+				.join('\n');
+			})
+			.join('\n');
+
+
 		it('#complete', function()
 		{
-			var info = i18nc(code);
-			var outputJSON = requireAfterWrite('v1_complete.json', autoTestUtils.JsonOfI18ncRet(info));
+			var info = i18nc(code,
+				{
+					dbTranslateWords: dbTranslateWords,
+				});
+
+			// JSON.stringify 的时候，会把数组里面的 undefined 转成 null
+			// 这里就提前转一下
+			// 导致的结果是，生成的json文件里面，是null表示
+			var myJSON = JSON.parse(JSON.stringify(autoTestUtils.JsonOfI18ncRet(info)));
+			var outputJSON = requireAfterWrite('v1_complete.json', myJSON);
 			var otherCode = requireAfterWrite('v1_complete.js', info.code);
 
-			expect(autoTestUtils.JsonOfI18ncRet(info)).to.eql(outputJSON);
+			expect(myJSON).to.eql(outputJSON);
 			expect(autoTestUtils.code2arr(''+info)).to.eql(autoTestUtils.code2arr(otherCode));
 		});
 
@@ -24,6 +62,7 @@ describe('#jsoncode upgrade', function()
 		{
 			var info = i18nc(code,
 				{
+					dbTranslateWords: dbTranslateWords,
 					I18NHandler:
 					{
 						upgrade: {checkVersion: false}
